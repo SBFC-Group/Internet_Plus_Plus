78b606d1b3f992daea7f086bc969dd78b3c3b928
sebastian2007bro <sebastianstogsamling@gmail.com>
2025-07-05 22:04:04 +0200

    Small changes
    Change internal from "Interplusplus4-0New" to Internetplusplus

--- Interplusplus4-0New/MainResourceFile.Designer.vb
+++ Interplusplus4-0New/MainResourceFile.Designer.vb
@@ -43,7 +43,7 @@ Namespace My.Resources
         Friend Shared ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Interplusplus4_0New.MainResourceFile", GetType(MainResourceFile).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Internetplusplus.MainResourceFile", GetType(MainResourceFile).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
--- Interplusplus4-0New/My Project/Application.Designer.vb
+++ Interplusplus4-0New/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Interplusplus4_0New.Internetplusplus
+            Me.MainForm = Global.Internetplusplus.Internetplusplus
         End Sub
     End Class
 End Namespace
